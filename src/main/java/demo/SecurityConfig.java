0a80f2885e40071d0a3ccecd396dbb5c03741940
Stephane Nicoll <snicoll@pivotal.io>
2017-05-16 15:02:39 +0300

    Actuator demonstration
    The actuator provides a set of DevOps feature to your application. As
    you may have guessed by now, adding a starter is all that you need to
    get started (`spring-boot-starter-actuator`).
    
    For security reasons, only a limited set of features are exposed
    automatically: the `info` and `health` endpoint and the latter only
    shows the aggregated status. We would like to expose all endpoints so
    we need to configure Spring Boot to do that for us. Every
    auto-configuration defines a number of customization properties.
    
    We now need to find the key that can be used to list the web endpoints
    to expose. Most IDEs have Spring Boot support and if you type `expsure`
    in the `application.properties` you'll see a content assistance popup
    showing keys matching that criteria.
    
    Once you've set `management.endpoints.web.exposure.include=*` you can
    browse the various endpoints offered by the actuator: `/env`, `/info`,
    `/metrics`, `/trace`, `/dump`, `/beans`, `/mappings`, etc.

--- src/main/java/demo/SecurityConfig.java
+++ src/main/java/demo/SecurityConfig.java
@@ -1,12 +1,29 @@
 package demo;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class)).permitAll()
+				.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("HERO")
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+				.anyRequest().authenticated()
+				.and().formLogin()
+				.and().httpBasic();
+	}
 
 	@Bean
 	@SuppressWarnings("deprecation")
