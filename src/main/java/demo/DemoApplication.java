ac85fd93ab0350410b593e90397f245bfb291a3e
Stephane Nicoll <snicoll@pivotal.io>
2017-05-16 15:10:27 +0300

    Devtools demonstration
    Spring Boot provides development tools that are particularly interesting
    when working in your IDE. If you add `spring-boot-devtools`, the
    application is automatically tuned for local development:
    
    * All template engine caches will be disabled.
    * If H2 is present the h2 console will be exposed at `/h2-console`.
    * A LiveReload server will be started as part of the app, allowing you
    to connect to your app using the free LiveReload browser plugin.
    
    More importantly, Spring Boot watches your files and every time
    the classpath changes it either refreshes the browser (via LiveReload)
    or restart the application and refresh the browser when it's done.
    
    Concretely, if you are working on a template engine or a static resource
    and that one changes, the browser will refresh automatically. IF you
    change anything else (code, configuration, etc), the application will
    restart to apply those changes.
    
    If you try to create an error page in `static/error/404.html`, you'll
    notice any unmapped URI will redirect to that page. We could also add
    a custom health indicator and show that `/health` shows it once we've
    build the project.

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package demo;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public HealthIndicator myHealthIndicator() {
+		return () -> {
+			if (new Random().nextBoolean()) {
+				return Health.up().build();
+			}
+			else {
+				return Health.down().withDetail("booooooo", 42).build();
+			}
+		};
 	}
 
 }
